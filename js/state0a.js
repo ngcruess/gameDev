54e0a2c13a52f60dd04da546715d847899c45c8e
ngcruess <ngc375@utexas.edu>
2017-10-05 15:48:35 -0500

    Collision working with polygons

--- js/state0a.js
+++ js/state0a.js
@@ -16,19 +16,19 @@ demo.state0a.prototype = {
         game.stage.backgroundColor = '#B25F55';
                 
         game.physics.p2.gravity.y = 1000;
-        game.physics.p2.defaultRestitution = 0;
+        game.physics.p2.restitution = 0;
         game.physics.p2.world.setGlobalStiffness(1e5);
         
         //mittens = game.add.sprite(164,365, 'mittens');
         mittens = game.add.sprite(400,400,'mittens');
         //mittens.animations.add('walkRight', [2]);
         //mittens.animations.add('walkLeft', [1]);        
-        mittens.scale.setTo(0.15, 0.15);
-        game.physics.p2.enable(mittens);
+        //mittens.scale.setTo(0.15, 0.15);
+        game.physics.p2.enable(mittens, true);
         mittens.body.fixedRotation = true;
         //mittens.body.setZeroDamping();
-        //mittens.body.clearShapes();
-        //mittens.body.loadPolygon('mittensPhysicsData', 'mittens');
+        mittens.body.clearShapes();
+        mittens.body.loadPolygon('mittensPhysicsData', 'mittens', 1, -Math.PI * 2);
         /*
         vacuum = game.add.sprite(700,400,'vacuum');
         vacuum.anchor.x = 0.5;
