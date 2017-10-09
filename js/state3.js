b937d6bf1ce3be05987d2819978a95b04a1c282d
johnmnovak <31937632+johnmnovak@users.noreply.github.com>
2017-10-09 13:31:17 -0500

    livingroom

--- js/state3.js
+++ js/state3.js
@@ -1,42 +1,119 @@
-var centerX = 300, centerY = 200, vel = 100, jumpvel = -300, sock, mittens, socksKilled = 0;
+var centerX =  vel = 100, jumpvel = -300, sock, mittens, socksKilled = 0;
 
 demo.state3 = function() {};
 demo.state3.prototype = {
     preload: function() {
         game.load.spritesheet('evilSock', '../assets/spritesheets/EvilSock.png', 3200, 3200);
-        game.load.spritesheet('mittens', '../assets/spritesheets/BatCat.png', 400, 320);
-        game.load.image('sidetable', '../assets/sprites/SideTable.png');
-        game.load.image('table', '../assets/sprites/Table.png');
-        game.load.image('shelf', '../assets/sprites/Shelf.png');
+        game.load.spritesheet('mittens', '../assets/spritesheets/BatCat.png', 100, 80);
+        game.load.image('sidetable', '../assets/images/SideTable.png');
+        game.load.image('table', '../assets/images/Table.png');
+        game.load.image('shelf', '../assets/images/Shelf.png');
         game.load.image('hitzone', '../assets/sprites/hitbox.png');
+        
+        game.load.image('wall', '../assets/images/livingroomwall.png');
+        game.load.image('sky', '../assets/images/sky.png');
+        game.load.physics('mittensPhysicsData', '../assets/polygons/mittensSingleFrame.json');
     },
     create: function() {
-        game.stage.backgroundColor = '#3d7c48';
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-        game.world.setBounds(0, 0, 2500, 600);
-        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;   
         
-        platforms = game.add.group();
-        platforms.enableBody = true;
+                             // P2 PHYSICS AND WORLD //
+        ////////////////////////////////////////////////////
+        game.world.setBounds(0, 0, 3000, 800);
+        game.stage.backgroundColor = '#B25F55';
+        game.physics.startSystem(Phaser.Physics.P2JS);
+        game.physics.p2.gravity.y = globalGravity;
+        game.physics.p2.restitution = 0;
+        game.physics.p2.world.setGlobalStiffness(1e5);
         
-        enemies = game.add.group();
-        enemies.enableBody = true;
         
+        var sky = game.add.sprite(0, 0, 'sky');
+        sky.scale.setTo(10, 1.5);
+        
+        var wall = game.add.sprite(0,0, 'wall');
+        wall.scale.setTo(10, 10);
+        ////////////////////////////////////////////////////
+        
+        
+                            // KEYBOARD //
+        ////////////////////////////////////////////////////
+        // CALLBACK FOR DOUBLE JUMP
+        game.input.keyboard.onUpCallback = function (e) {
+            console.log(e.keyCode)
+            if (e.keyCode == 38){
+                jumpRel = true;
+            } 
+        }  
+        
+        cursor = game.input.keyboard.createCursorKeys();
+        shootButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        ////////////////////////////////////////////////////
+        
+        
+                        // MITTENS //
+        ///////////////////////////////////////////////////
         mittens = game.add.sprite(0,0, 'mittens');
-        mittens.scale.setTo(0.2, 0.2);
+        updateAnchor(0.5, 0.5, mittens);
+        game.physics.p2.enable(mittens, false);
+        mittens.body.fixedRotation = true;
+        //mittens.body.setZeroDamping();
+        mittens.body.clearShapes();
+        mittens.body.loadPolygon('mittensPhysicsData', 'mittensSingleFrame', 1, -Math.PI * 2);
+        game.camera.follow(mittens);
+        ///////////////////////////////////////////////////
+        
+        
+                       // PLATFORMS //
+        ///////////////////////////////////////////////////
+        var platformMaterial = game.physics.p2.createMaterial('platformMaterial');
+        
+        platform = game.add.sprite(600, 700, 'table');
+        updateAnchor(.5, 1, platform);
+        platform.scale.setTo(0.15, 0.15);
+        game.physics.p2.enable(platform);
+        platform.body.setMaterial(platformMaterial);
+        platform.body.static = true;
+        
+        platform = game.add.sprite(1000, 350, 'shelf');
+        updateAnchor(.5, 1, platform);
+        platform.scale.setTo(0.15, 0.15);
+        game.physics.p2.enable(platform);
+        platform.body.setMaterial(platformMaterial);
+        platform.body.static = true;
+        
+        platform = game.add.sprite(1400, 700, 'sidetable');
+        updateAnchor(.5, 1, platform);
+        platform.scale.setTo(0.15, 0.15);
+        game.physics.p2.enable(platform);
+        platform.body.setMaterial(platformMaterial);
+        platform.body.static = true;
+        
+        platform = game.add.sprite(1800, 350, 'shelf');
+        updateAnchor(.5, 1, platform);
+        platform.scale.setTo(0.15, 0.15);
+        game.physics.p2.enable(platform);
+        platform.body.setMaterial(platformMaterial);
+        platform.body.static = true;
+        
+  
+        
+        ///////////////////////////////////////////////////
+        
+        
+        /* ARCADE STUFF
         mittens.frame = 2;
         game.physics.enable(mittens);
         mittens.body.gravity.y = 300;
         mittens.body.collideWorldBounds = true;
         mittens.animations.add('walkRight', [2,3]);
         mittens.animations.add('walkLeft', [0,1]);
+        */
         
-        game.camera.follow(mittens);
-        
+        //var sideTable = platforms.create(550, 400, 'sidetable');
+        /*
         var sideTable = platforms.create(550, 400, 'sidetable');
         sideTable.anchor.setTo(0.5, 1);
         sideTable.body.immovable = true;
-        sideTable.scale.setTo(0.07, 0.07);
+        sideTable.scale.setTo(0.07, 0.07)7
         
         var shelf = platforms.create(500, 306, 'shelf');
         shelf.anchor.setTo(0.5,1);
@@ -75,10 +152,39 @@ demo.state3.prototype = {
         game.physics.enable(sock2);
         sock2.body.gravity.y = 300;
         sock2.body.collideWorldBounds = true;
+        */
+        
+        
+        
         
                 
     },
     update: function() {
+        if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            mittensFacingLeft = false;
+            mittens.body.moveRight(mittensRunSpeed);
+            mittens.scale.setTo(1, 1);
+        }
+        else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            mittensFacingLeft = true;
+            mittens.body.moveLeft(mittensRunSpeed);
+            mittens.scale.setTo(-1, 1);
+        }
+        else{
+            mittens.body.velocity.x = 0;
+            mittens.animations.stop();
+        }
+        if (cursor.up.isDown && jumpRel) {
+            mittensJump();
+            jumpRel = false;
+        }        
+        if (shootButton.isDown) {
+            mittensShoot();
+        }
+        
+        
+        
+        /*
         mittens.body.velocity.x = 0;
         game.physics.arcade.collide(mittens, platforms);
         game.physics.arcade.collide(enemies, platforms);
@@ -105,5 +211,14 @@ demo.state3.prototype = {
             socksKilled += 1;
             enemies.kill();
         }
+        */
     }
 };
+function mittensHit(body, bodyB, shapeA, shapeB, equation) {
+    if (body == null) {
+        return
+    }
+    if (body.sprite.key == 'platform') {
+        mittens.reset(0,0);
+    }
+}
